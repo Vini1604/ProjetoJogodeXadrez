c859be221b0fb934ab07731429fcb50e69a38dff
Vinicius Santos <vinicius.oliveira1998@hotmail.com>
2020-12-26 11:56:56 -0300

    Comecando a colocar pecas no tabuleiro

--- xadrez-console/Tabuleiro/Peca.cs
+++ xadrez-console/Tabuleiro/Peca.cs
@@ -10,8 +10,8 @@ namespace tabuleiro {
         public int qteMovimento { get; set; }
         public Tabuleiro tab { get; protected set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab) {
-            this.posicao = posicao;
+        public Peca(Tabuleiro tab, Cor cor) {
+            this.posicao = null;
             this.cor = cor;
             this.tab = tab;
             this.qteMovimento = 0;
